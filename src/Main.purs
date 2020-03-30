542c853592d9a99e887947162d09dbae7661a8d6
Glitch (hello-express) <none>
2020-03-30 20:24:17 +0000

    🐫👱 Checkpoint ./src/Main.purs:13543546/391

--- src/Main.purs
+++ src/Main.purs
@@ -2,7 +2,14 @@ module Main where
 
 import Prelude
 import Effect (Effect)
+import Data.Generic.Rep (class Generic)
 import Effect.Console (log)
+
+
+data Suit = Hearts | Diamonds | Spades | Clubs
+
+derive instance genericSuit :: Generic Suit _
+derive instance showSuit :: Show Suit
 
 main :: Effect Unit
 main = do
