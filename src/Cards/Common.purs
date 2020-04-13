ac98ac6f831a2830686f6feafd9ffd591311e932
Susan Potter <me@susanpotter.net>
2020-04-13 13:39:42 -0500

    Add more common data types that will be shared across frontend and backend

--- src/Cards/Common.purs
+++ src/Cards/Common.purs
@@ -10,3 +10,26 @@ data Suit = Hearts | Diamonds | Spades | Clubs
 derive instance genericSuit :: Generic Suit _
 instance showSuit :: Show Suit where show = genericShow
 
+data CardColor = Red | Black
+
+derive instance genericCardColor :: Generic CardColor _
+instance showCardColor :: Show CardColor where show = genericShow
+
+data Rank
+  = Ace
+  | King
+  | Queen
+  | Jack
+  | Ten
+  | Nine
+  | Eight
+  | Seven
+  | Six
+  | Five
+  | Four
+  | Three
+  | Two
+
+derive instance genericRank :: Generic Rank _
+instance showRank :: Show Rank where show = genericShow
+
