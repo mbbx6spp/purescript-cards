04abd0d68609ff284f6444c3b79d3b825ae3d91b
Glitch (hello-express) <none>
2020-04-13 12:59:36 -0500

    Provide backend server loop via 'yarn server'

--- src/Cards/Backend.purs
+++ src/Cards/Backend.purs
@@ -1,25 +1,31 @@
 module Cards.Backend (main) where
 
 import Prelude
-import Control.Monad (void)
-import Data.Unit (Unit)
-import Data.Maybe (Maybe (..))
 import Effect (Effect)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Node.HTTP (Server)
-import Node.Process (lookupEnv)
 import Node.Express.App (App, listenHttp, get, use)
-import Node.Express.Handler (Handler)
+import Node.Express.Handler (Handler, next)
+import Node.Express.Request (getOriginalUrl, getMethod)
 import Node.Express.Response (sendJson, setStatus)
 
+-- our own middleware
+logger :: Handler
+logger = do
+  url <- getOriginalUrl
+  method <- getMethod
+  liftEffect $ log (">>> " <> show method <> " " <> url)
+  next
+
 indexHandler :: Handler
-indexHandler = sendJson { status: "OK" }
+indexHandler = do
+  setStatus 200
+  sendJson { status: "OK" }
 
 setupRoutes :: App
 setupRoutes = do
   liftEffect $ log "Setting up routes"
-  
+  use logger
   get "/" indexHandler
 
 main :: Effect Unit
---
+++ src/server.js
@@ -0,0 +1,1 @@
+require('../output/Cards.Backend').main();
